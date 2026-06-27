d2ee8449a574dd3d3381afdf9de89ba7d847762f
Tuncay Yıldırım <tuncyyldrm@gmail.com>
2026-06-28 01:47:45 +0300

    update

--- DB/index.sql
+++ DB/index.sql
@@ -1,9 +1,40 @@
 USE [ONEDB]
 GO
 
--- ===================================================================
--- 📌 1. [dbo].[islem] TABLOSU İNDEKSLERİ
--- ===================================================================
+/* ===================================================================
+   ❌ ADIM 1: DİNAMİK SQL İLE GEREKSİZ / ESKİ İNDEKSLERİ OTOMATİK TEMİZLE
+   =================================================================== */
+DECLARE @DropStatements NVARCHAR(MAX) = N'';
+
+-- Silinecek indekslerin DROP komutlarını dinamik olarak tek bir metinde topluyoruz
+SELECT @DropStatements = @DropStatements + 
+    N'DROP INDEX [' + i.name + N'] ON [dbo].[' + t.name + N'];' + CHAR(13) + CHAR(10)
+FROM sys.indexes i
+INNER JOIN sys.tables t ON i.object_id = t.object_id
+WHERE t.name IN ('stok', 'islem')
+  AND i.type > 1            -- 0: Heap, 1: Clustered (Tablo iskeletlerini koruyoruz)
+  AND i.is_primary_key = 0  -- Primary Key kısıtlamalarını es geçiyoruz
+  AND i.name IS NOT NULL;
+
+-- Eğer temizlenecek indeks bulunduysa tek hamlede çalıştırıyoruz
+IF LEN(@DropStatements) > 0
+BEGIN
+    PRINT '--- Eski/Mükerrer İndeksler Otomatik Olarak Temizleniyor ---';
+    EXEC sp_executesql @DropStatements;
+    PRINT '✅ Eski non-clustered indekslerin tamamı başarıyla temizlendi!';
+END
+ELSE
+BEGIN
+    PRINT 'ℹ️ Temizlenecek herhangi bir eski non-clustered indeks bulunamadı.';
+END
+GO
+
+
+/* ===================================================================
+   ✨ ADIM 2: EN DOĞRU VE EN SAĞLIKLI NİHAİ İNDEKSLERİ OLUŞTUR
+   =================================================================== */
+
+-- --- 1. [dbo].[islem] Tablosu ---
 
 -- CLUSTERED INDEX (Fiziksel Sıralama)
 IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name = 'CX_Islem_IslemId_Fiziksel' AND object_id = OBJECT_ID('[dbo].[islem]'))
@@ -12,7 +43,7 @@ BEGIN
 END
 GO
 
--- NONCLUSTERED INDEX: IX_Islem_DetayKodu_Bakiye_Optimize
+-- NONCLUSTERED INDEX: Stok hareket analizini ve bakiye hesabını uçuran optimize indeks
 IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name = 'IX_Islem_DetayKodu_Bakiye_Optimize' AND object_id = OBJECT_ID('[dbo].[islem]'))
 BEGIN
     CREATE NONCLUSTERED INDEX [IX_Islem_DetayKodu_Bakiye_Optimize]
@@ -21,7 +52,7 @@ BEGIN
 END
 GO
 
--- NONCLUSTERED INDEX: IX_Islem_IslemNumarasi_Covering
+-- NONCLUSTERED INDEX: Fatura detay raporunu ve veri joinlerini kaplayan covering indeks
 IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name = 'IX_Islem_IslemNumarasi_Covering' AND object_id = OBJECT_ID('[dbo].[islem]'))
 BEGIN
     CREATE NONCLUSTERED INDEX [IX_Islem_IslemNumarasi_Covering]
@@ -33,7 +64,7 @@ BEGIN
 END
 GO
 
--- NONCLUSTERED INDEX: IX_Islem_Islemnumarasi_B2B_Covering
+-- NONCLUSTERED INDEX: Alternatif B2B entegrasyon covering indeksi
 IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name = 'IX_Islem_Islemnumarasi_B2B_Covering' AND object_id = OBJECT_ID('[dbo].[islem]'))
 BEGIN
     CREATE NONCLUSTERED INDEX [IX_Islem_Islemnumarasi_B2B_Covering]
@@ -43,9 +74,7 @@ END
 GO
 
 
--- ===================================================================
--- 📌 2. [dbo].[stok] TABLOSU İNDEKSLERİ
--- ===================================================================
+-- --- 2. [dbo].[stok] Tablosu ---
 
 -- CLUSTERED INDEX (Fiziksel Sıralama)
 IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name = 'CX_Stok_UrunKodu_Fiziksel' AND object_id = OBJECT_ID('[dbo].[stok]'))
@@ -54,7 +83,7 @@ BEGIN
 END
 GO
 
--- NONCLUSTERED INDEX: IX_stok_B2B_Arama_Ultra_Hizli
+-- NONCLUSTERED INDEX: Tüm parça arama, filtreleme, OEM yakalama ve grup listelerini tek başına sırtlayan nihai ana indeks
 IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name = 'IX_stok_B2B_Arama_Ultra_Hizli' AND object_id = OBJECT_ID('[dbo].[stok]'))
 BEGIN
     CREATE NONCLUSTERED INDEX [IX_stok_B2B_Arama_Ultra_Hizli]
@@ -66,11 +95,9 @@ END
 GO
 
 
--- ===================================================================
--- 📌 3. [dbo].[vw_StokListesi] INDEXED VIEW İNDEKSLERİ
--- ===================================================================
+-- --- 3. [dbo].[vw_StokListesi] Indexed View Bağlantıları ---
 
--- Eski denemeler varsa temizleyelim (Burada IF EXISTS zaten güvenliği sağlıyor)
+-- View indekslerini sıfırlıyoruz (Hata vermemesi için önce drop edilmelidir)
 DROP INDEX IF EXISTS UX_vw_StokListesi_urunkodu ON [dbo].[vw_StokListesi];
 DROP INDEX IF EXISTS IX_vw_StokListesi_B2B_Arama_Nihai ON [dbo].[vw_StokListesi];
 GO
